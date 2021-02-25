5ba9769a0ab58376cd1331046b5bc97a269af91a
dahmaniRiad <41478333+dahmaniRiad@users.noreply.github.com>
2021-02-25 21:23:10 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,39 @@
+#!/usr/bin/env python
+
 import socket
 from datetime import datetime
+import subprocess
+import json
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(('', 9999))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('', 9999))
 while True:
-        socket.listen(10)
-        client, address = socket.accept()
-        print("{} connected".format( address ))
-        response = client.recv(255).decode('ascii')
-        print(response)
-        if "Checker file" in response:
-	        now = datetime.now()
-	        dt_string = now.strftime("%d.%m.%Y-%H.%M.%S")
-	        with open("CheckerFrom-"+address[0]+'-'+dt_string+'.txt', 'wb') as file:
-		        l = client.recv(1024)
-		        while (l):
-			        file.write(l)
-			        l = client.recv(1024)
-		if "Help needed" in response :
-			results = []
-			order = {"order": {"cmd": "ls -la", "msg": "Operating System", "results": ""}}
-		    client.send(order)
+		s.listen(10)
+		client, address = s.accept()
+		print("{} connected".format( address ))
+		response = client.recv(255).decode('ascii').lower()
+		print(response)
+		if "checker" in response:
+			print("checker")
+			now = datetime.now()
+			dt_string = now.strftime("%d.%m.%Y-%H.%M.%S")
+			with open("CheckerFrom-"+address[0]+'-'+dt_string+'.txt', 'wb') as file:
+				l = client.recv(1024)
+				while (l):
+					file.write(l)
+					l = client.recv(1024)
 
-
-
-
-
+		elif "help" in response:
+			client.send('getline'.encode('ascii'))
+			response=client.recv(1024).decode('ascii')
+			print(response)
+			cli=input("open/close ?")
+			if "open" in cli.lower():
+				client.send("open".encode('ascii'))
+			elif "close" in cli.lower():
+				client.send("close".encode('ascii'))
 
 client.close()
 stock.close()
